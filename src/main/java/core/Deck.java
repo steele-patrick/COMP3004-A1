9b272e49e93c17f67edd52b2d82d38ec38c70b65
steele-patrick <PatrickSteele@cmail.carleton.ca>
2018-09-18 22:02:45 -0400

    log: response to unit test - implemented shuffle - implemented top - implement deck init - note: deck can now deal cards

--- src/main/java/core/Deck.java
+++ src/main/java/core/Deck.java
@@ -1,8 +1,39 @@
 package core;
 
-import java.util.Collections;
+import java.util.*;
 
 public class Deck extends Pile{
+	Map<String, Integer> cardList = new HashMap<String, Integer>();	//List of legal cards for the Deck
+	
+	public Deck() {
+		//Need to give relative path for \resources\Standard_52.csv
+		String path = "\\path";
+		this.initCardList(path);
+		
+		cardList.forEach((k,v)->{
+			if(v == 11) { this.cards.add(new Ace(k)); }
+			else { this.cards.add(new Card(k, v)); }
+			this.size++;
+		});
+	}
+	
+	//Want: initialize the Deck's cardList with cards from a file
+	private void initCardList(String path) {
+			cardList.put("2S", 2);
+			cardList.put("3S", 3);
+			cardList.put("4S", 4);
+			cardList.put("5S", 5);
+			cardList.put("6S", 6);
+			cardList.put("7S", 7);
+			cardList.put("8S", 8);
+			cardList.put("9S", 9);
+			cardList.put("10S", 10);
+			cardList.put("JS", 10);
+			cardList.put("QS", 10);
+			cardList.put("KS", 10);
+			cardList.put("AS", 11);
+		}
+
 	
 	//Randomly permutes linked list cards
 	public int shuffle() {
